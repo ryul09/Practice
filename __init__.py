e38fa2e324343f034fc7353df3f90f65dec6fa22
ryul09 <79358130+ryul09@users.noreply.github.com>
2021-04-12 15:49:59 +0900

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from ui.ui import *
+
+class Main():
+    def __init__(self):
+        print("실행할 메인 클래스")
+        UI_class()    ### 함수나 class는 () 를 꼭 써줘야 실행된다!
+
+if __name__=="__main__":
+    Main()
