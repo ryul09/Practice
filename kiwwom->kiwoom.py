ca64b931c93141550d1b66678eaeaeb463025b28
ryul09 <79358130+ryul09@users.noreply.github.com>
2021-04-12 17:25:49 +0900

    Update kiwwom->kiwoom.py

--- kiwwom->kiwoom.py
+++ kiwwom->kiwoom.py
@@ -10,21 +10,34 @@ class Kiwoom(QAxWidget):
 
         #### evnet loop 모음
         self.login_event_loop = None
+        self.detail_account_info_event_loop = None
+        self.detail_account_info_event_loop_2 =None
         #################################
 
         #### 변수 모음
         self.account_num = None
         #################################
-        self.get_ocx_instance()
+
+        #### 계좌 관련 변수
+        self.use_money = 0
+        self.use_money_percent = 0.5
+
+
+
+        self.get_ocx_instance()   #OCX, 응용 프로그램에서 open API를 실행할 수 있게 한거다, 제어 가능!
         self.event_slot()
+
         self.signal_login_commConnect()
-        self.get_acount_info()
+        self.get_account_info()
+        self.detail_account_info() # 예수금 가져오기
+        self.detail_account_mystock() #계좌평가 잔고 요청
 
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")  ##응용 프로그램 제어!
 
     def event_slot(self):
         self.OnEventConnect.connet(self.login_slot)
+        self.OnreceiveTrData.connect(self.trdata_slot)
 
     def login_slot(self, errCode):
         print(errors(errCode))
@@ -35,7 +48,7 @@ class Kiwoom(QAxWidget):
         self.dynamicCall("CommConnect()")
 
         self.login_event_loop = QEventLoop()
-        self.login_event_loop.exec_()  ## 종료 안되게끔!!!
+        self.login_event_loop.exec_()  ## 종료 안되게끔!!! One Thread loop에서 작업 충돌을 막기 위해 필요!!
 
     def get_acount_info(self):
         account_list = self.dynamicCall("GetLogininfo(String)", "ACCNO")
@@ -43,3 +56,91 @@ class Kiwoom(QAxWidget):
         self.account_num = account_list.split(";")[0]
 
         print("나의 보유 계좌번호 %s", self.account_num)
+
+    def detail_account_info(self):
+        print("예수금을 요청하는 부분")
+
+        self.dynamicCall("SetInputValue(String, String)", "계좌번호", self.account_num)
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호", "0000")
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호입력매채구분", "00")
+        self.dynamicCall("SetInputValue(String, String)", "조회구분", "2")
+        self.dynamicCall("CommRqData(String, String, int, String)", "예수금상세현황요청", "opw00001", "0", "2000")
+
+        self.detail_account_info_event_loop = QEventLoop()
+        self.detail_account_info_event_loop.exec_()  ##예수금 요청 하는 동안에 block 처리, event loop 동시성 처리 필요
+
+        #### Screen No. --> 총 200개까지 가능 한 screen no.에 100개 항목까지 저장 가능
+    def detail_account_mystock(self, sPrevNext="0"):
+        print("계좌평가 잔고내역 요청")
+        self.dynamicCall("SetInputValue(String, String)", "계좌번호", self.account_num)
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호", "0000")
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호입력매채구분", "00")
+        self.dynamicCall("SetInputValue(String, String)", "조회구분", "2")
+        self.dynamicCall("CommRqData(String, String, int, String)", "계좌평가잔고내역요청", "opw00018", sPrevNext, "2000")
+
+        self.detail_account_info_event_loop_2 = QEventLoop()
+        self.detail_account_info_event_loop_2.exec_()
+
+    def trdata_slot(self, sScNo, sRQName, sTrCode, sRecordName, sPrevNext):
+        '''
+        tr요청을 받는 구역이다! 슬롯이다!
+        :param sScNo: 스크린번호
+        :param sRQName: 내가 요청했을 때 지은 이름
+        :param sTrCode: 요청 id, tr코드
+        :param sRecordName: 사용 안함
+        :param sPrevNext: 다음 페이지가 있는지
+        :return:
+        '''
+
+        if sRQName == "예수금상세현황요청":
+            deposit = self.dynamicCall("GetCommData(String, String, int, String)",sTrCode, sRQName, 0, "예수금")
+            print("예수금 %s" % type(deposit))
+            print("예수금 형변환 %s" % int(deposit))
+
+            self.use_money = int(deposit) * self.use_money_percent  ##예수금은 계속 사용하는 변수이므로 추가 등록 후 관리
+            self.use_money = self.use_money / 4
+
+            ok_deposit = self.dynamicCall("GetCommData(String, String, int, String)",sTrCode, sRQName, 0, "출금가능금액")
+            print("출금 가능 금액 %s" % ok_deposit)
+            print("예수금 형변환 %s" % int(ok_deposit))
+
+            self.detail_account_info_event_loop.exit()  ###event loop 종료, 다음 작업 할 수 있도록
+
+        if sRQName == "계좌평가잔고내역요청" :
+            total_buy_money = self.dynamicCall("GetCommData(String, String, int, String)",sTrCode, sRQName, 0, "총매입금액")
+            total_buy_money_result = int(total_buy_money)
+
+            print("총매입금액 %s " % total_buy_money_result)
+
+            total_profit_loss_rate = self.dynamicCall("GetCommData(String, String, int, String)",sTrCode, sRQName, 0, "총수익률(%)")
+            total_profit_loss_rate_result = float(total_profit_loss_rate)
+
+            print("총수익률(%s) : %s" % ("%", total_profit_loss_rate_result)  # % 중복으로 인한 별도 표시 필요, 참고!
+
+            rows = self.dynamicCall("GetRepeatCnt(QString, QString)", sTrCode, sRQName)
+            cnt = 0
+            for i in range(rows):
+                code = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, cnt, "종목번호")
+                ## A : 장내주식, J : ELW 종목, Q : ETN 종목
+                code=code.strip()[1:]  ##  ex. "A2309" [1:] = "2309", [:1] = "A"
+                code_nm = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "종목명")
+                stock_quantity = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "보유수량")
+                buy_price = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i,"매입가")
+                learn_rate = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i,"수익률(%)")
+                current_price = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "현재가")
+                total_chegual_price = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "매입급액")
+                possible_quantity = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "매매가능수량")
+
+                code_nm = code_nm.strip()
+                stock_quantity = int(stock_quantity.strip())
+                buy_price = int(buy_price.strip())
+                learn_rate = float(learn_rate.strip())
+                current_price = int(current_price.strip())
+                total_chegual_price = int(total_chegual_price.strip())
+
+
+
+
+            self.detail_account_info_event_loop_2.exit()
+
+
